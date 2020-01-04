bfce973d0f3a422463bed625a86946fa379d0554
Rachel Yoo <RACHEL@MacBook-Air.local>
2020-01-04 10:48:03 -0800

    server file added

---
+++ server/routes/api.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const shelterController = require('../controllers/shelterController');
+
+const router = express.Router();
+
+router.get('/', shelterController.getShelters, (req, res) => {
+  res.status(200).json(res.locals.shelters);
+});
+
+module.exports = router;
